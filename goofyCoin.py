43d90702561a01c1fe1ca61aa3a998f115def911
Elias Lundmark <Elias.Lundmark@episerver.com>
2018-06-17 18:30:11 +0200

    created newUser class

--- goofyCoin.py
+++ goofyCoin.py
@@ -1,3 +1,19 @@
 import hashlib
 import datetime as date
 from Crypto.PublicKey import RSA
+
+class NewUser:
+
+
+    def __init__(self):
+        key = RSA.generate(2048)
+        self.privKey = key.export_key()
+        self.pubKey = key.publickey().export_key()
+        self.wallet = []
+        self.coinAmount = len(self.wallet)
+
+class Coin:
+
+
+    pass
+
